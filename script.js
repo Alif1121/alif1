2c49627bf6dde5cb84192732251f6deea31a509f
Alif1121 <alif2018rangpur@gmail.com>
2025-06-11 01:11:20 +0600

    Update23script.js

--- script.js
+++ script.js
@@ -4,28 +4,6 @@
 
 /*---- dark mood -------*/
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".mode-toggle");
-  const body = document.body;
-
-  // Default to light mode
-  body.classList.add("light-mode");
-
-  toggleButton.addEventListener("click", () => {
-    if (body.classList.contains("light-mode")) {
-      body.classList.remove("light-mode");
-      body.classList.add("dark-mode");
-      toggleButton.textContent = "☀️";
-    } else {
-      body.classList.remove("dark-mode");
-      body.classList.add("light-mode");
-      toggleButton.textContent = "🌙";
-    }
-  });
-});
-
-
-
 
 /*----top down menu -------*/
 
